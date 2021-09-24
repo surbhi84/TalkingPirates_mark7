8fda7fb75a7e27345ef6e68eb26775dce8bd2376
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-25 04:19:09 +0530

    feat: eventHandler() defined

--- TalkingPirates.js
+++ TalkingPirates.js
@@ -1,3 +1,18 @@
 inputText = document.querySelector("#input-txt");
 translateBtn = document.querySelector("#translate-btn");
 output = document.querySelector("#output-div");
+
+translateBtn.addEventListener("click", eventHandler);
+
+function eventHandler() {
+  var textInput = inputText.value;
+  output.style.display = "block";
+
+  fetch(constructUrl(textInput))
+    .then((response) => response.json())
+    .then((json) => {
+      var outputText = json.contents.translated;
+      output.innerText = outputText;
+    })
+    .catch(errorHandler);
+}
